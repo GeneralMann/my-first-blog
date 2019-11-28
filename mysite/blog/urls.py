c17a98c820cde80eed9eafbe3f7a444519ad1a8f
GeneralMann# <george.cobb@gmail.com>
2019-11-28 11:27:15 +0000

    Added CSS styling and started an about me page

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('about_me/', views.about_me , name='about_me'),
 ]

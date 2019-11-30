02c44e22b3e1880ee6634cdb88d7212365c102fd
GeneralMann# <george.cobb@gmail.com>
2019-11-30 14:09:36 +0000

    Added the options to create and edit posts, and basic security

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('about_me/', views.about_me , name='about_me'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]

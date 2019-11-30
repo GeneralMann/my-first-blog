355fe89ccc56f7b3000a6d770d49e9031e88b0be
GeneralMann# <george.cobb@gmail.com>
2019-11-30 14:35:59 +0000

    Updated the publishing settings and required a log in to access those options

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
